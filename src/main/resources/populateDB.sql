fe0b3b0f91be144e456bc4652bbdf08571ffc824
Frolov Vladimir <frolov_vovan@mail.ru>
2022-05-20 11:02:59 +0300

    OneToOne

--- src/main/resources/populateDB.sql
+++ src/main/resources/populateDB.sql
@@ -1,9 +1,9 @@
-INSERT INTO PUBLIC.BOOK (NAME, PRINT_YEAR)
+INSERT INTO NEW_SCHEMA.BOOK (NAME, PRINT_YEAR)
 VALUES ('First book', 2010),
        ('Second book', 2011),
        ('Third book', 2012);
 
-INSERT INTO PUBLIC.AUTHOR (FIRST_NAME, SECOND_NAME, BOOK_ID)
+INSERT INTO NEW_SCHEMA.AUTHOR (FIRST_NAME, SECOND_NAME, BOOK_ID)
 VALUES ('Pablo', 'Lambado', 1),
        ('Pazo', 'Zopa', 2),
        ('Lika', 'Vika', 3);
